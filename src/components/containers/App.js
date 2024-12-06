521a87d4b39bb1529c99ab773b49f2dde14ee221
nisarg <hammerbyte.nisarg@gmail.com>
2024-12-07 02:34:32 +0530

    added routes back

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -21,90 +21,82 @@ export default function App() {
     return (
         <ProviderToast>
             <NeedDeviceID>
-                {/* <ProcessToken>
-                    <Routes>
-                        <Route index element={<Dashboard />} />
-                        <Route
-                            path="/profile"
-                            element={
-                                <HasAuthentication>
-                                    <Profile />
-                                </HasAuthentication>
-                            }
-                        />
-                        <Route
-                            path="/help"
-                            element={
-                                <HasAuthentication>
-                                    <Help />
-                                </HasAuthentication>
-                            }
-                        />
-                        <Route
-                            path="/manage-firm"
-                            element={
-                                <HasRequiredGroup
-                                    allowedGroups={["FADMIN", "HADMIN"]}
-                                >
-                                    <p>Firm Admin Management</p>
-                                </HasRequiredGroup>
-                            }
-                        />
-                        <Route
-                            path="/manage-system"
-                            element={
-                                <HasRequiredGroup allowedGroups={["HADMIN"]}>
-                                    <p>System Admin Management</p>
-                                </HasRequiredGroup>
-                            }
-                        />
-                        <Route
-                            path="/login"
-                            element={
-                                <HasNoAuthentication>
-                                    <Login />
-                                </HasNoAuthentication>
-                            }
-                        />
-                        <Route
-                            path="/contact"
-                            element={
-                                <HasNoAuthentication>
-                                    <Contact />
-                                </HasNoAuthentication>
-                            }
-                        />
-
-                        <Route path="/products">
-                            <Route index element={<Products />} />
+                {
+                    <ProcessToken>
+                        <Routes>
+                            <Route index element={<Dashboard />} />
                             <Route
-                                path="category/:category"
-                                element={<Products />}
+                                path="/profile"
+                                element={
+                                    <HasAuthentication>
+                                        <Profile />
+                                    </HasAuthentication>
+                                }
                             />
-                            <Route path=":id">
-                                <Route index element={<Product />} />
-                                <Route path="demo" element={<Demo />} />
+                            <Route
+                                path="/help"
+                                element={
+                                    <HasAuthentication>
+                                        <Help />
+                                    </HasAuthentication>
+                                }
+                            />
+                            <Route
+                                path="/manage-firm"
+                                element={
+                                    <HasRequiredGroup allowedGroups={["FADMIN", "HADMIN"]}>
+                                        <p>Firm Admin Management</p>
+                                    </HasRequiredGroup>
+                                }
+                            />
+                            <Route
+                                path="/manage-system"
+                                element={
+                                    <HasRequiredGroup allowedGroups={["HADMIN"]}>
+                                        <p>System Admin Management</p>
+                                    </HasRequiredGroup>
+                                }
+                            />
+                            <Route
+                                path="/login"
+                                element={
+                                    <HasNoAuthentication>
+                                        <Login />
+                                    </HasNoAuthentication>
+                                }
+                            />
+                            <Route
+                                path="/contact"
+                                element={
+                                    <HasNoAuthentication>
+                                        <Contact />
+                                    </HasNoAuthentication>
+                                }
+                            />
+
+                            <Route path="/products">
+                                <Route index element={<Products />} />
+                                <Route path="category/:category" element={<Products />} />
+                                <Route path=":id">
+                                    <Route index element={<Product />} />
+                                    <Route path="demo" element={<Demo />} />
+                                </Route>
                             </Route>
-                        </Route>
 
-                        <Route
-                            path="/my-learning"
-                            element={
-                                <HasAuthentication>
-                                    <p>My Learning</p>
-                                </HasAuthentication>
-                            }
-                        ></Route>
+                            <Route
+                                path="/my-learning"
+                                element={
+                                    <HasAuthentication>
+                                        <p>My Learning</p>
+                                    </HasAuthentication>
+                                }
+                            ></Route>
 
-                        <Route path="/forbidden" element={<Forbidden />} />
-                        <Route
-                            path="*"
-                            element={
-                                <CustomError highlight="OOPS ! We could't Find What You Were Looking For." />
-                            }
-                        />
-                    </Routes>
-                </ProcessToken> */}
+                            <Route path="/forbidden" element={<Forbidden />} />
+                            <Route path="*" element={<CustomError highlight="OOPS ! We could't Find What You Were Looking For." />} />
+                        </Routes>
+                    </ProcessToken>
+                }
             </NeedDeviceID>
         </ProviderToast>
     );
