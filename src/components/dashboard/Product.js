9b2128e497bc7c92ecd10f73ee7f681635e6f376
Abhishek Jadav <abhijadav99@gmail.com>
2024-12-10 21:04:14 +0530

    CSS Style Changes Product
    CSS Style Changes Product

--- src/components/dashboard/Product.js
+++ src/components/dashboard/Product.js
@@ -5,13 +5,13 @@ export default function Product({ product }) {
     const navigate = useNavigate();
 
     return (
-        <div className="flex gap-4 p-2">
+        <div className="flex gap-4 p-3">
             <img className="border-round m-0 p-0 shadow-4" src={product.image} alt={product.name} />
             <div className="flex-1">
                 <p className="font-bold text-sm m-0 p-0">{product.name}</p>
-                <p className="m-0 p-0 text-sm">Pricing</p>
-                <Button className="text-xs p-2" label="Buy Now" severity="info" raised />
-                <Button className="text-xs p-2" label="More Details" severity="info" text onClick={() => navigate(`/products/courses/${product.id}`)} />
+                <p className="m-0 p-0 mt-2 text-sm">Pricing</p>
+                <Button className="text-xs mt-2 p-2" label="Buy Now" severity="info" raised />
+                <Button className="text-xs mt-2 p-2" label="More Details" severity="info" text onClick={() => navigate(`/products/courses/${product.id}`)} />
             </div>
         </div>
     );
