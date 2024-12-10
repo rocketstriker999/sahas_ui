9f90d8a94e8942edbaccaa2047f90ac84e48b059
Abhishek Jadav <abhijadav99@gmail.com>
2024-12-11 03:09:14 +0530

    Added new CSS changes
    Added new CSS changes

--- src/components/content_player/ContentSelector.js
+++ src/components/content_player/ContentSelector.js
@@ -15,41 +15,87 @@ export default function ContentSelector({ content, setMediaPlayer }) {
         { label: "Audios", icon: "pi pi-headphones" },
     ];
     return (
+        // <Fragment>
+        //     <TabMenu model={tabItems} activeIndex={activeTab} onTabChange={(e) => setActiveTab(e.index)}></TabMenu>
+        //     {activeTab === 0 &&
+        //         (content.videos.length > 0 ? (
+        //             content.videos.map((video) => (
+        //                 <div onClick={() => setMediaPlayer(<PlayerVideo source={video.urls} />)}>
+        //                     <p>{video.title}</p>
+        //                     <Divider />
+        //                 </div>
+        //             ))
+        //         ) : (
+        //             <NoContent />
+        //         ))}
+        //     {activeTab === 1 &&
+        //         (content.pdfs.length > 0 ? (
+        //             content.pdfs.map((pdf) => (
+        //                 <div onClick={() => setMediaPlayer(<PlayerPDF source={pdf.url} />)}>
+        //                     <p>{pdf.title}</p>
+        //                     <Divider />
+        //                 </div>
+        //             ))
+        //         ) : (
+        //             <NoContent />
+        //         ))}
+        //     {activeTab === 2 &&
+        //         (content.audios.length > 0 ? (
+        //             content.audios.map((audio) => (
+        //                 <div onClick={() => setMediaPlayer(<PlayerAudio source={audio.url} />)}>
+        //                     <p>{audio.title}</p>
+        //                     <Divider />
+        //                 </div>
+        //             ))
+        //         ) : (
+        //             <NoContent />
+        //         ))}
+        // </Fragment>
+
         <Fragment>
-            <TabMenu model={tabItems} activeIndex={activeTab} onTabChange={(e) => setActiveTab(e.index)}></TabMenu>
-            {activeTab === 0 &&
-                (content.videos.length > 0 ? (
-                    content.videos.map((video) => (
-                        <div onClick={() => setMediaPlayer(<PlayerVideo source={video.urls} />)}>
-                            <p>{video.title}</p>
-                            <Divider />
-                        </div>
-                    ))
-                ) : (
-                    <NoContent />
-                ))}
-            {activeTab === 1 &&
-                (content.pdfs.length > 0 ? (
-                    content.pdfs.map((pdf) => (
-                        <div onClick={() => setMediaPlayer(<PlayerPDF source={pdf.url} />)}>
-                            <p>{pdf.title}</p>
-                            <Divider />
-                        </div>
-                    ))
-                ) : (
-                    <NoContent />
-                ))}
-            {activeTab === 2 &&
-                (content.audios.length > 0 ? (
-                    content.audios.map((audio) => (
-                        <div onClick={() => setMediaPlayer(<PlayerAudio source={audio.url} />)}>
-                            <p>{audio.title}</p>
-                            <Divider />
-                        </div>
-                    ))
-                ) : (
-                    <NoContent />
-                ))}
+            <TabMenu model={tabItems} activeIndex={activeTab} onTabChange={(e) => setActiveTab(e.index)}
+                className="mb-4 surface-card"></TabMenu>
+            <div className="px-3">
+                {activeTab === 0 &&
+                    (content.videos.length > 0 ? (
+                        content.videos.map((video) => (
+                            <div
+                                className="p-3 mb-3 surface-card border-round shadow-1 flex justify-content-between align-items-center"
+                                onClick={() => setMediaPlayer(<PlayerVideo source={video.urls} />)}>
+                                <span className="text-lg font-bold">{video.title}</span>
+                                <i className="pi pi-play text-primary"></i>
+                            </div>
+                        ))
+                    ) : (
+                        <NoContent />
+                    ))}
+                {activeTab === 1 &&
+                    (content.pdfs.length > 0 ? (
+                        content.pdfs.map((pdf) => (
+                            <div className="p-3 mb-3 surface-card border-round shadow-1 flex justify-content-between align-items-center"
+                            onClick={() => setMediaPlayer(<PlayerPDF source={pdf.url} />)}>
+                                <span className="text-lg font-bold">{pdf.title}</span>
+                                <i className="pi pi-file-pdf text-primary"></i>
+                            </div>
+                        ))
+                    ) : (
+                        <NoContent />
+                    ))}
+                {activeTab === 2 &&
+                    (content.audios.length > 0 ? (
+                        content.audios.map((audio) => (
+                            <div
+                            className="p-3 mb-3 surface-card border-round shadow-1 flex justify-content-between align-items-center"
+                             onClick={() => setMediaPlayer(<PlayerAudio source={audio.url} />)}>
+                                <span className="text-lg font-bold">{audio.title}</span>
+                                <i className="pi pi-headphones text-primary"></i>
+                            </div>
+                        ))
+                    ) : (
+                        <NoContent />
+                    ))}
+            </div>
+
         </Fragment>
     );
 }
--- src/components/product/Courses.js
+++ src/components/product/Courses.js
@@ -28,17 +28,42 @@ export default function Courses() {
 
     if (courseList?.length > 0) {
         return (
+            // <Fragment>
+            //     <p className="text-base font-bold px-3">{`Offering ${courseList.length} Courses`}</p>
+            //     {courseList.map((course, index) => (
+            //         <div key={course.id} onClick={() => navigate(`/products/course/${course.id}`)} className="mx-3" >
+            //             {index === 0 && <Divider className="p-0 m-0 mb-3" />}
+            //             <p className="text-sm font-bold m-1 mb-2">{course.title}</p>
+            //             <div className="flex align-items-center m-1">
+            //                 <i className="pi pi-book mr-1 text-primary text-sm"></i>
+            //                 <span className="text-sm">{course.subjects.length} Subjects</span>
+            //             </div>
+            //             <Divider className="p-0 m-0 mt-3" />
+            //         </div>
+            //     ))}
+            // </Fragment>
+
             <Fragment>
-                <p>{`Offering ${courseList.length} Courses`}</p>
-                {courseList.map((course, index) => (
-                    <div key={course.id} onClick={() => navigate(`/products/course/${course.id}`)}>
-                        {index === 0 && <Divider className="p-0 m-0" />}
-                        <p className="p-0 m-0 font-bold text-sm">{course.title}</p>
-                        <p className="p-0 m-0  text-xs">{course.subjects.length} Subjects</p>
-                        <Divider className="p-0 m-0" />
+                <p className="text-base font-bold px-3">{`Offering ${courseList.length} Courses`}</p>
+                {courseList.map((course) => (
+                    <div
+                        key={course.id}
+                        onClick={() => navigate(`/products/course/${course.id}`)}
+                        className="p-3 border-round surface-card shadow-3 mb-3 mx-3 flex align-items-center justify-content-between"
+                    >
+                        <div className="flex flex-column">
+                            <p className="text-sm font-bold m-0 mb-2">{course.title}</p>
+                            <div className="flex align-items-center">
+                                <i className="pi pi-book mr-2 text-primary text-sm"></i>
+                                <span className="text-sm">{course.subjects.length} Subjects</span>
+                            </div>
+                        </div>
+                        <i className="pi pi-angle-right text-primary"></i>
                     </div>
                 ))}
             </Fragment>
+
+
         );
     }
     return <NoContent />;
