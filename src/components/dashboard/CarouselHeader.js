09d23073c7deb2616e7421ec90ec4e6525f8a70f
nisarg <hammerbyte.nisarg@gmail.com>
2024-12-11 00:26:47 +0530

    profile

--- src/components/dashboard/CarouselHeader.js
+++ src/components/dashboard/CarouselHeader.js
@@ -1,20 +1,56 @@
 import { Galleria } from "primereact/galleria";
 import { Image } from "primereact/image";
+import { useSelector } from "react-redux";
+import { Avatar } from "primereact/avatar";
+import { OverlayPanel } from "primereact/overlaypanel";
+import { useRef } from "react";
+import { ListBox } from "primereact/listbox";
+import { Divider } from "primereact/divider";
+import { Menu } from "primereact/menu";
 
 export default function CarouselHeader() {
     const itemTemplate = (image) => {
         return <Image width="100%" src={image} />;
     };
 
+    const loggedInUser = useSelector((state) => state.stateUser.user);
+
     const images = ["https://www.gstatic.com/webp/gallery3/1.png", "https://www.gstatic.com/webp/gallery3/1.png"];
+
+    const profileMenu = useRef(null);
+
+    const items = [
+        {
+            label: "Profile",
+            items: [
+                {
+                    label: "Edit",
+                    icon: "pi pi-cog",
+                    command: () => console.log("dawdaw"),
+                },
+                {
+                    label: "Edit",
+                    icon: "pi pi-cog",
+                    command: () => console.log("dawdaw"),
+                },
+                {
+                    label: "Logout",
+                    icon: "pi pi-sign-out",
+                },
+            ],
+        },
+    ];
 
     return (
         <div>
-            <p className="w-full lg:w-6 bg-primary text-white font-bold m-0 py-3 px-2 shadow-4">Welcome {true ? "Nisarg" : "To Sahas Smart Studies"}</p>
+            <div className="w-full lg:w-6 text-white p-3 shadow-4 bg-primary-800 flex justify-content-between align-items-center">
+                <p className="  font-bold m-0  ">Welcome {loggedInUser?.name} To Sahas</p>
+                {loggedInUser && <Avatar icon="pi pi-user" className="bg-primary-900" shape="circle" onClick={(e) => profileMenu.current.toggle(e)} />}
+                <Menu model={items} popup ref={profileMenu} />
+            </div>
             <Galleria
-                className="w-full lg:w-6"
+                className="w-full lg:w-6 shadow-4"
                 value={images}
-                style={{ maxWidth: "450px" }}
                 showThumbnails={false}
                 showIndicators
                 showIndicatorsOnItem={true}
--- src/components/dashboard/MyCourses.js
+++ src/components/dashboard/MyCourses.js
@@ -1,3 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentUser } from "../../redux/sliceUser";
+
 export default function MyCourses() {
-    return <p>My Courses</p>;
+    const dispatch = useDispatch();
+
+    const loggedInUser = useSelector((state) => state.stateUser.user);
+
+    useEffect(() => {
+        dispatch(setCurrentUser({ name: "Nisarg" }));
+    }, []);
+
+    if (loggedInUser) {
+        return <p>You are user {loggedInUser.name}</p>;
+    } else {
+        return <p>Login To Continue</p>;
+    }
 }
