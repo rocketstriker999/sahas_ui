5f536d40787c8938b857d5d7d425a3ff6f9ea50b
Abhishek Jadav <abhijadav99@gmail.com>
2024-12-11 23:03:11 +0530

    Added Image Green Tick and Payment Successful
    Added Image Green Tick and Payment Successful Page

--- src/pages/PaymentLoading.js
+++ src/pages/PaymentLoading.js
@@ -1,8 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import { Card } from 'primereact/card';
 import { ProgressSpinner } from 'primereact/progressspinner';
+import { Image } from 'primereact/image';
 import { classNames } from 'primereact/utils';
+import src from "./greenTick.png";
 
 export default function PaymentLoading() {
+    const [isPaymentSuccessful, setPaymentSuccessful] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setPaymentSuccessful(true);
+        }, 3000);
+        }, []);
     return (
         <Card
             className="flex align-items-center justify-content-center text-center min-h-screen"
@@ -10,13 +20,24 @@ export default function PaymentLoading() {
                 body:classNames("shadow-5")
             }}
         >
+              {isPaymentSuccessful ? (
+                <Image
+                    src={src}
+                    alt="Payment Successful"
+                    width="100"
+                    height="100"
+                />
+            ) : (
             <ProgressSpinner
                 style={{ width: '50px', height: '50px' }}
                 strokeWidth="4"
                 animationDuration=".7s"
             />
-            <p className="mt-4 font-semibold text-base">
-                Validating your payment status
+            )}
+            <p className="font-semibold text-base w-15rem">
+                {isPaymentSuccessful
+                    ? "Payment Successful"
+                    : "Validating your payment status"}
             </p>
         </Card>
     );
