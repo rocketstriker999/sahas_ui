58a052137f3e54ae8770d60abac7a6f43e6aaf39
nisarg <hammerbyte.nisarg@gmail.com>
2024-12-13 00:03:53 +0530

    All Products - dynamic api binding

--- src/components/dashboard/AllProducts.js
+++ src/components/dashboard/AllProducts.js
@@ -1,87 +1,54 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Accordion, AccordionTab } from "primereact/accordion";
 import { classNames } from "primereact/utils";
 import { Divider } from "primereact/divider";
 import Product from "./Product";
+import { requestAPI } from "../../utils";
+import { ProgressSpinner } from "primereact/progressspinner";
 
 export default function AllProducts() {
-    const [tabs] = useState([
-        {
-            category_name: "10th",
-            products: [
-                {
-                    id: 1,
-                    title: "ChatGPT Complete Guide: Learn actionable",
-                    image: "https://placehold.co/100x100/yellow/000000/png",
-                    price: 99,
-                    discount: 10,
-                    totalHours: 12,
-                    lectures: 50,
-                },
-                {
-                    id: 1,
-                    title: "This Would Be Any Product 2",
-                    image: "https://placehold.co/100x100/green/FFFFFF/png",
-                    price: 99,
-                    discount: 10,
-                    totalHours: 12,
-                    lectures: 50,
-                },
-                {
-                    id: 1,
-                    title: "ChatGPT Complete Guide: Learn actionable",
-                    image: "https://placehold.co/100x100/yellow/000000/png",
-                    price: 99,
-                    discount: 10,
-                    totalHours: 12,
-                    lectures: 50,
-                },
-            ],
-        },
-        {
-            category_name: "11th",
-            products: [
-                {
-                    id: 1,
-                    title: "ChatGPT Complete Guide: Learn actionable",
-                    image: "https://placehold.co/100x100/blue/FFFFFF/png",
-                    price: 99,
-                    discount: 10,
-                    totalHours: 12,
-                    lectures: 50,
-                },
-                {
-                    id: 1,
-                    title: "This Would Be Any Product 2",
-                    image: "https://placehold.co/100x100/yellow/FFFFFF/png",
-                    price: 99,
-                    discount: 10,
-                    totalHours: 12,
-                    lectures: 50,
-                },
-            ],
-        },
-    ]);
+    const [catelogue, setCatelogue] = useState();
+    const [loading, setLoading] = useState();
 
-    return (
-        <Accordion className="m-3">
-            {tabs.map((tab) => (
-                <AccordionTab
-                    className="m-0"
-                    header={tab.category_name}
-                    pt={{
-                        root: { className: classNames("mb-2"), key: tab.category_name },
-                        content: { className: classNames("p-0") },
-                    }}
-                >
-                    {tab.products.map((product, position) => (
-                        <Fragment>
-                            {position !== 0 && <Divider className="p-0 m-0" />}
-                            <Product product={product} />
-                        </Fragment>
-                    ))}
-                </AccordionTab>
-            ))}
-        </Accordion>
-    );
+    useEffect(() => {
+        //hit API Once
+        requestAPI({
+            requestPath: "products/catelogue",
+            setLoading: setLoading,
+            onResponseReceieved: (catelogue, responseCode) => {
+                if (catelogue && responseCode === 200) {
+                    setCatelogue(catelogue);
+                }
+            },
+        });
+    }, []);
+
+    if (loading) {
+        return <ProgressSpinner />;
+    }
+
+    if (catelogue) {
+        return (
+            <Accordion className="m-3">
+                {catelogue.map((category) => (
+                    <AccordionTab
+                        key={"dawda"}
+                        className="m-0"
+                        header={category.title}
+                        pt={{
+                            root: { className: classNames("mb-2") },
+                            content: { className: classNames("p-0") },
+                        }}
+                    >
+                        {category.products.map((product, position) => (
+                            <div key={product.id}>
+                                {position !== 0 && <Divider className="p-0 m-0" />}
+                                <Product product={product} />
+                            </div>
+                        ))}
+                    </AccordionTab>
+                ))}
+            </Accordion>
+        );
+    }
 }
