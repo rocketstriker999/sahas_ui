3761b3b6b68c6e780d6c17c80570d5e19358010e
Abhishek Jadav <abhijadav99@gmail.com>
2024-12-29 23:26:54 +0530

    Fix CSS Issue
    Fix CSS Issue

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -35,7 +35,7 @@ export default function Product() {
                         <h1 className="text-white font-bold m-0 mt-4 lg:text-3xl text-base">{product?.title}</h1>
                         <p className="lg:text-base text-xs text-white m-0 mt-3 line-height-3">{product?.description}</p>
                     </div>
-                    <div className="flex flex-column gap-2">
+                    <div className="flex flex-column gap-3">
                         <img className="border-round m-0 p-0 shadow-4" width="100" height="100" src={`${process.env.REACT_APP_IMAGES_PUBLIC}${product.image}`} alt="Product" />
                         {product.access ? <Button label="Receipt" severity="info" raised /> : <ButtonPurchase productId={productId} />}
                     </div>
