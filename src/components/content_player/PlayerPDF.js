45258b71087c53b81278a5e8f4116b334d38014c
nisarg <hammerbyte.nisarg@gmail.com>
2025-01-11 03:09:15 +0530

    Minor Bug Fix

--- src/components/content_player/PlayerPDF.js
+++ src/components/content_player/PlayerPDF.js
@@ -4,6 +4,6 @@ import { saveAs } from "file-saver";
 import { Button } from "primereact/button";
 import { pdfjs } from "react-pdf";
 
-export default function PlayerPDF(pdf) {
+export default function PlayerPDF({ pdf }) {
     return <iframe src={`/media/private/pdfs/${pdf.gd_id}`} width="100%" height="600px" />;
 }
