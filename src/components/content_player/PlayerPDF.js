1f58f460e2116807752a577eca0c8849855ab423
nisarg <hammerbyte.nisarg@gmail.com>
2025-01-11 03:14:49 +0530

    fxed imports

--- src/components/content_player/PlayerPDF.js
+++ src/components/content_player/PlayerPDF.js
@@ -1,8 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Document, Page } from "react-pdf";
-import { saveAs } from "file-saver";
-import { Button } from "primereact/button";
-import { pdfjs } from "react-pdf";
 
 export default function PlayerPDF({ pdf }) {
     return <iframe src={`/media/private/pdfs/${pdf.gd_id}`} width="100%" height="600px" />;
