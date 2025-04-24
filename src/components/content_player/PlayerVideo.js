27aa2df31676fe0ec91d41b82f8f25ee20031ee7
nisarg <hammerbyte.nisarg@gmail.com>
2025-04-25 01:34:12 +0530

    changed logic for handling urls

--- src/components/content_player/PlayerVideo.js
+++ src/components/content_player/PlayerVideo.js
@@ -10,7 +10,6 @@ export default function PlayerVideo({ mediaItem }) {
     const videoRef = useRef(null); // Reference to the video element
     const [loading, setLoading] = useState();
     const [sources, setSources] = useState();
-    const [error, setError] = useState();
     const { selector, id } = useParams();
     const navigate = useNavigate();
 
@@ -18,52 +17,57 @@ export default function PlayerVideo({ mediaItem }) {
         requestAPI({
             requestPath: `extract/${selector}/${id}/${mediaItem?.id}`,
             setLoading: setLoading,
-            onRequestFailure: setError,
-            onResponseReceieved: (sources, responseCode) => (sources && responseCode === 200 ? setSources(sources) : navigate("/forbidden")),
+            onRequestFailure: setSources({ are_broken: true }),
+            onResponseReceieved: (sources, responseCode) =>
+                sources && responseCode === 200 ? setSources({ are_broken: false, urls: sources }) : navigate("/forbidden"),
         });
     }, [id, mediaItem, navigate, selector]);
-
-    if (loading) {
-        return <Loading />;
-    }
 
     const fetchSourcesWithCacheSkip = () => {
         requestAPI({
             requestPath: `extract/${selector}/${id}/${mediaItem?.id}`,
             requestGetQuery: { skip_cache: true },
             setLoading: setLoading,
-            onRequestFailure: setError,
-            onResponseReceieved: (sources, responseCode) => (sources && responseCode === 200 ? setSources(sources) : navigate("/forbidden")),
+            onRequestFailure: setSources({ are_broken: true }),
+            onResponseReceieved: (sources, responseCode) =>
+                sources && responseCode === 200 ? setSources({ are_broken: false, urls: sources }) : navigate("/forbidden"),
         });
     };
 
-    return sources?.length && !error ? (
-        <video
-            onLoadedData={() => {
-                console.log("Playing Correctly");
-                setError(false);
-            }}
-            onError={() => {
-                console.log("Error");
-                setError(true);
-            }}
-            onPlay={() => playBackTimes[mediaItem.id] && (videoRef.current.currentTime = playBackTimes[mediaItem.id])}
-            width="100%"
-            ref={videoRef}
-            controls
-            autoPlay
-            controlsList="nodownload"
-            onTimeUpdate={() => {
-                playBackTimes[mediaItem.id] = videoRef.current.currentTime;
-                setPlayBackTimes(playBackTimes);
-            }}
-            onContextMenu={(e) => e.preventDefault()}
-        >
-            {sources.map((source) => (
-                <source key={source.url} src={source.url} type="video/mp4"></source>
-            ))}
-        </video>
-    ) : (
-        <NoContent error="Couldn't Stream The Media" retry={fetchSourcesWithCacheSkip} />
-    );
+    if (loading) {
+        return <Loading />;
+    }
+
+    if (sources.are_broken) {
+        return <NoContent error="Couldn't Stream The Media" retry={fetchSourcesWithCacheSkip} />;
+    }
+
+    if (sources?.length)
+        return (
+            <video
+                onLoadedData={() => {
+                    console.log("Playing Correctly");
+                    setSources((prev) => ({ ...prev, are_broken: false }));
+                }}
+                onError={() => {
+                    console.log("Error");
+                    setSources({ are_broken: true });
+                }}
+                onPlay={() => playBackTimes[mediaItem.id] && (videoRef.current.currentTime = playBackTimes[mediaItem.id])}
+                width="100%"
+                ref={videoRef}
+                controls
+                autoPlay
+                controlsList="nodownload"
+                onTimeUpdate={() => {
+                    playBackTimes[mediaItem.id] = videoRef.current.currentTime;
+                    setPlayBackTimes(playBackTimes);
+                }}
+                onContextMenu={(e) => e.preventDefault()}
+            >
+                {sources.urls.map((source) => (
+                    <source key={source.url} src={source.url} type="video/mp4"></source>
+                ))}
+            </video>
+        );
 }
