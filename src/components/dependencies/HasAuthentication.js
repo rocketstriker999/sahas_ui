af49fe6d4b9f879bffa27faca2d86bf8ba93284d
nisarg <hammerbyte.nisarg@gmail.com>
2025-07-15 02:57:51 +0530

    dev deploy

--- src/components/dependencies/HasAuthentication.js
+++ src/components/dependencies/HasAuthentication.js
@@ -1,13 +1,44 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Authentication from "../../pages/Authentication";
 import { KEY_AUTHENTICATION_TOKEN } from "../../constants";
+import { useEffect, useCallback } from "react";
+import { useAppContext } from "../../providers/ProviderAppContainer";
+import { setCurrentUser } from "../../redux/sliceUser";
 
 export default function HasAuthentication({ children }) {
-    //if we authentication token already then let's verify and get the information
-
-    //if we don't have it then it will show authentication page
+    const { requestAPI, applicationLoading, setApplicationLoading } = useAppContext();
 
     const loggedInUser = useSelector((state) => state.stateUser.user);
 
-    return loggedInUser && localStorage.getItem(KEY_AUTHENTICATION_TOKEN) ? children : <Authentication />;
+    const dispatch = useDispatch();
+
+    const clearAuthenticationToken = useCallback(() => localStorage.removeItem(KEY_AUTHENTICATION_TOKEN), []);
+
+    //check if token already exist and we can verify it
+    useEffect(() => {
+        if (!applicationLoading && !loggedInUser && localStorage.getItem(KEY_AUTHENTICATION_TOKEN)) {
+            requestAPI({
+                requestPath: "authentication-tokens",
+                onRequestFailure: clearAuthenticationToken,
+                setLoading: (loading) => setApplicationLoading(loading ? { message: "Validating Authentication Token..." } : loading),
+                onResponseReceieved: (user, responseCode) => {
+                    if (user && responseCode === 200) {
+                        dispatch(setCurrentUser(user));
+                    } else {
+                        clearAuthenticationToken();
+                    }
+                },
+            });
+        }
+    }, [applicationLoading, clearAuthenticationToken, dispatch, loggedInUser, requestAPI, setApplicationLoading]);
+
+    //if user is logged in and token is verified
+    if (loggedInUser) {
+        return children;
+    }
+
+    //if we don't have token , let's generate a token
+    if (!localStorage.getItem(KEY_AUTHENTICATION_TOKEN)) {
+        return <Authentication />;
+    }
 }
