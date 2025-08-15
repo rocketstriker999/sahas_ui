29255ea3fb081b88c015679ff7fefe1a25fefbbb
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-15 21:40:38 +0530

    added inquiry note support

--- src/components/manage_users/user/inquiries/DialogInquiryNotes.js
+++ src/components/manage_users/user/inquiries/DialogInquiryNotes.js
@@ -6,10 +6,14 @@ import Detail from "../../../common/Detail";
 import { Button } from "primereact/button";
 import { useAppContext } from "../../../../providers/ProviderAppContainer";
 
+import { InputText } from "primereact/inputtext";
+
 export default function DialogInquiryNotes({ selectedInquiryForNotes, setSelectedInquiryForNotes, setInquiries, getCourseTitle }) {
     const { requestAPI, showToast } = useAppContext();
 
     const [loading, setLoading] = useState();
+
+    const [note, setNote] = useState();
 
     const deleteInquiryNote = useCallback(
         (noteId) => {
@@ -21,15 +25,15 @@ export default function DialogInquiryNotes({ selectedInquiryForNotes, setSelecte
                 onResponseReceieved: (_, responseCode) => {
                     if (responseCode === 204) {
                         showToast({ severity: "success", summary: "Updated", detail: "Inquiry Note Deleted", life: 1000 });
-                        setInquiries((prev) => {
+                        setInquiries((prev) =>
                             prev?.map((inquiry) => {
                                 if (inquiry.id === selectedInquiryForNotes.id) {
                                     inquiry.notes = inquiry?.notes?.filter((note) => note.id !== noteId);
                                 }
 
                                 return inquiry;
-                            });
-                        });
+                            })
+                        );
                     } else {
                         showToast({ severity: "error", summary: "Failed", detail: "Failed To Deleted Inquiry Note !", life: 2000 });
                     }
@@ -38,6 +42,31 @@ export default function DialogInquiryNotes({ selectedInquiryForNotes, setSelecte
         },
         [requestAPI, selectedInquiryForNotes, setInquiries, showToast]
     );
+
+    const addInquiryNote = useCallback(() => {
+        requestAPI({
+            requestPath: `inquiry-notes/`,
+            requestMethod: "POST",
+            setLoading: setLoading,
+            requestPostBody: { note, inquiry_id: selectedInquiryForNotes.id },
+
+            onResponseReceieved: (inquiryNote, responseCode) => {
+                if (responseCode === 201) {
+                    showToast({ severity: "success", summary: "Added", detail: "Inquiry Note Added", life: 1000 });
+                    setInquiries((prev) =>
+                        prev?.map((inquiry) => {
+                            if (inquiry.id === selectedInquiryForNotes.id) {
+                                inquiry.notes = [inquiryNote, ...inquiry?.notes];
+                            }
+                            return inquiry;
+                        })
+                    );
+                } else {
+                    showToast({ severity: "error", summary: "Failed", detail: "Failed To Add Inquiry Note !", life: 2000 });
+                }
+            },
+        });
+    }, [note, requestAPI, selectedInquiryForNotes, setInquiries, showToast]);
 
     return (
         <Dialog
@@ -48,16 +77,31 @@ export default function DialogInquiryNotes({ selectedInquiryForNotes, setSelecte
         >
             <div className="pt-2">
                 {loading ? (
-                    <Loading message={"Deleting Inquiry Note"} />
-                ) : selectedInquiryForNotes?.notes?.length ? (
-                    selectedInquiryForNotes?.notes?.map(({ id, created_by_full_name, created_on, note }) => (
-                        <div key={id} className="flex align-items-start gap-2 mb-2">
-                            <Detail className={"flex-1 mb-2"} title={`${created_by_full_name} at ${created_on}`} value={note} />
-                            <Button className="w-2rem h-2rem" icon="pi pi-trash" rounded severity="danger" onClick={() => deleteInquiryNote(id)} />
-                        </div>
-                    ))
+                    <Loading message={"Updating Inquiry Note"} />
                 ) : (
-                    <NoContent error="No Notes Found" />
+                    <div className="flex flex-column gap-4">
+                        <div className="p-inputgroup flex-1">
+                            <InputText placeholder="Add Note" value={note} onChange={(e) => setNote(e.target.value)} />
+                            <Button icon="pi pi-plus" className="p-button-warning" onClick={addInquiryNote} />
+                        </div>
+                        <div>
+                            {selectedInquiryForNotes?.notes?.length ? (
+                                selectedInquiryForNotes.notes.map(({ id, created_by_full_name, created_on, note }) => (
+                                    <div key={id} className="flex align-items-start gap-2 mb-2">
+                                        <Detail
+                                            icon="pi pi-angle-right"
+                                            className="flex-1 mb-2"
+                                            title={`${created_by_full_name} at ${created_on}`}
+                                            value={note}
+                                        />
+                                        <Button className="w-2rem h-2rem" icon="pi pi-trash" rounded severity="danger" onClick={() => deleteInquiryNote(id)} />
+                                    </div>
+                                ))
+                            ) : (
+                                <NoContent error="No Notes Found" />
+                            )}
+                        </div>
+                    </div>
                 )}
             </div>
         </Dialog>
