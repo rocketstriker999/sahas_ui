72aa9279bc01c56ab95b41945595f3fa218cd8a4
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-30 05:20:06 +0530

    enrollment section completed

--- src/components/manage_users/user/Enrollments.js
+++ src/components/manage_users/user/Enrollments.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { useAppContext } from "../../../providers/ProviderAppContainer";
 import TabHeader from "../../common/TabHeader";
@@ -7,25 +7,19 @@ import { Divider } from "primereact/divider";
 import Loading from "../../common/Loading";
 import NoContent from "../../common/NoContent";
 import Enrollment from "./enrollments/Enrollment";
-import DialogAddCourse from "./enrollments/DialogAddCourse";
 import { Accordion, AccordionTab } from "primereact/accordion";
-import { Tag } from "primereact/tag";
-import { getReadableDate } from "../../../utils";
-import DialogAddTransaction from "./enrollments/DialogAddTransaction";
 import DialogAddEnrollment from "./enrollments/DialogAddEnrollment";
 import EnrollmentHead from "./enrollments/EnrollmentHead";
 
 export default function Enrollments() {
-    const { userId, courses, getCourseTitle, paymentTypes } = useOutletContext();
+    const { userId } = useOutletContext();
     const [enrollments, setEnrollments] = useState();
     const { requestAPI } = useAppContext();
     const [loading, setLoading] = useState();
     const [error, setError] = useState();
-
-    const [addingNewCouse, setAddingNewCourse] = useState();
-    const [addingEnrollment, setAddingEnrollments] = useState();
-
-    const [selectedEnrollmentForTransaction, setSelectedEnrollmentForTransaction] = useState();
+    const [dialogAddEnrollment, setDialogAddEnrollment] = useState({
+        visible: false,
+    });
 
     useEffect(() => {
         requestAPI({
@@ -43,13 +37,29 @@ export default function Enrollments() {
         });
     }, [requestAPI, userId]);
 
+    const closeDialogAddEnrollment = useCallback(() => {
+        setDialogAddEnrollment((prev) => ({ ...prev, visible: false }));
+    }, []);
+
     return (
         <div className="flex flex-column h-full min-h-0">
             <TabHeader
                 className={"px-3 pt-3"}
                 title="User's Enrollments"
                 highlights={[`Total - ${enrollments?.length} Enrollments`]}
-                actionItems={[<Button icon="pi pi-plus" severity="warning" onClick={setAddingEnrollments} />]}
+                actionItems={[
+                    <Button
+                        icon="pi pi-plus"
+                        severity="warning"
+                        onClick={() =>
+                            setDialogAddEnrollment((prev) => ({
+                                ...prev,
+                                visible: true,
+                                closeDialog: closeDialogAddEnrollment,
+                            }))
+                        }
+                    />,
+                ]}
             />
             <Divider />
             <div className="flex-1 min-h-0 px-3 pb-2 overflow-y-scroll gap-2 flex flex-column">
@@ -75,13 +85,7 @@ export default function Enrollments() {
                     <NoContent error={"No Enrollments Found"} />
                 )}
             </div>
-            <DialogAddEnrollment
-                userId={userId}
-                addingEnrollment={addingEnrollment}
-                setAddingEnrollments={setAddingEnrollments}
-                courses={courses}
-                setEnrollments={setEnrollments}
-            />
+            <DialogAddEnrollment {...dialogAddEnrollment} setEnrollments={setEnrollments} />
         </div>
     );
 }
--- src/components/manage_users/user/enrollments/DialogAddEnrollment.js
+++ src/components/manage_users/user/enrollments/DialogAddEnrollment.js
@@ -3,43 +3,47 @@ import TabHeader from "../../../common/TabHeader";
 import { useAppContext } from "../../../../providers/ProviderAppContainer";
 import { useCallback, useState } from "react";
 import { FloatLabel } from "primereact/floatlabel";
-import { Dropdown } from "primereact/dropdown";
 import { Button } from "primereact/button";
 import { Calendar } from "primereact/calendar";
 import { InputNumber } from "primereact/inputnumber";
 import { getWriteableDate } from "../../../../utils";
+import { useOutletContext } from "react-router-dom";
+import { MultiSelect } from "primereact/multiselect";
 
-export default function DialogAddEnrollment({ userId, addingEnrollment, setAddingEnrollments, courses, setEnrollments }) {
+export default function DialogAddEnrollment({ visible, closeDialog, setEnrollments }) {
     const { requestAPI, showToast } = useAppContext();
+    const { userId, courses } = useOutletContext();
+
     const [loading, setLoading] = useState();
 
     const [enrollment, setEnrollment] = useState();
 
     const addTransaction = useCallback(() => {
         requestAPI({
-            requestPath: `users/${userId}/enrollments`,
+            requestPath: `enrollments`,
             requestMethod: "POST",
             requestPostBody: {
                 ...enrollment,
+                user_id: userId,
                 start_date: getWriteableDate({ date: enrollment?.start_date, removeTime: true }),
                 end_date: getWriteableDate({ date: enrollment?.end_date, removeTime: true }),
             },
             setLoading: setLoading,
-            onResponseReceieved: (enrollments, responseCode) => {
-                if (responseCode === 201) {
+            onResponseReceieved: (enrollment, responseCode) => {
+                if (enrollment && responseCode === 201) {
                     showToast({ severity: "success", summary: "Added", detail: "Enrollment Added", life: 1000 });
-                    setEnrollments(enrollments);
+                    setEnrollments((prev) => [enrollment, ...prev]);
                     setEnrollment(); //reset this form
-                    setAddingEnrollments(() => false); //close the dialog
+                    closeDialog(); //close the dialog
                 } else {
                     showToast({ severity: "error", summary: "Failed", detail: "Failed To Add Enrollment !", life: 2000 });
                 }
             },
         });
-    }, [enrollment, requestAPI, setAddingEnrollments, setEnrollments, showToast, userId]);
+    }, [closeDialog, enrollment, requestAPI, setEnrollments, showToast, userId]);
 
     return (
-        <Dialog header={`Add New Enrollment`} visible={addingEnrollment} className="w-11" onHide={() => setAddingEnrollments(false)}>
+        <Dialog header={`Add New Enrollment`} visible={visible} className="w-11" onHide={closeDialog}>
             <TabHeader
                 className="pt-3"
                 title="Add New Course Enrollment"
@@ -47,13 +51,13 @@ export default function DialogAddEnrollment({ userId, addingEnrollment, setAddin
             />
 
             <FloatLabel className="mt-5">
-                <Dropdown
-                    value={enrollment?.course}
+                <MultiSelect
+                    value={enrollment?.courses}
                     inputId="courses"
                     options={courses}
                     optionLabel="title"
                     className="w-full"
-                    onChange={(e) => setEnrollment((prev) => ({ ...prev, course: e.value }))}
+                    onChange={(e) => setEnrollment((prev) => ({ ...prev, courses: e.value }))}
                     disabled={loading}
                 />
                 <label htmlFor="courses">Courses</label>
