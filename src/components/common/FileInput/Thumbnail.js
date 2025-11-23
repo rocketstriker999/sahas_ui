624a6807bceba3d484d5c877dcb03ee57a5156ac
nisarg <hammerbyte.nisarg@gmail.com>
2025-11-23 14:23:29 +0530

    dev deploy

--- src/components/common/FileInput/Thumbnail.js
+++ src/components/common/FileInput/Thumbnail.js
@@ -1,9 +1,9 @@
 export default function Thumbnail({ type, preview }) {
     if (type === "video") {
-        return <video src={preview} alt="success" className="w-full " />;
+        return <video src={preview} alt="success" className="w-8rem " />;
     }
 
     if (type === "image") {
-        return <img className="border-circle w-full" src={preview} alt="Preview" style={{ objectFit: "cover" }} />;
+        return <img className="border-circle w-8rem" src={preview} alt="Preview" style={{ height: "6rem", objectFit: "cover" }} />;
     }
 }
