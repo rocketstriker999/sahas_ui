bb09d9bfc41a1e207a25cc9949d03d3c5ac93afa
nisarg <hammerbyte.nisarg@gmail.com>
2025-12-22 15:51:41 +0530

    7 days fixes

--- src/pages/Revenue.js
+++ src/pages/Revenue.js
@@ -6,7 +6,7 @@ import moment from "moment";
 
 export default function Revenue() {
     const [dates, setDates] = useState([
-        moment().subtract(7, "days").toDate(), // Start date (180 days ago)
+        moment().subtract(7, "days").toDate(), // Start date (7 days ago)
         moment().endOf("day").toDate(), // End date (Today)
     ]);
 
