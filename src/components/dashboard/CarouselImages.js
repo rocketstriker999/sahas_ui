613da4ad206ce74532b498c8a5b9a82443473816
Raj Gohel <rajgohel2018@gmail.com>
2026-01-14 12:07:10 +0530

    refactor: Responsiveness of Dashboard

--- src/components/dashboard/CarouselImages.js
+++ src/components/dashboard/CarouselImages.js
@@ -9,46 +9,87 @@ import HasRequiredAuthority from "../dependencies/HasRequiredAuthority";
 import { AUTHORITIES } from "../../constants";
 
 export default function CarouselImages({ className, images }) {
-    const [dialogAddCarouselItem, setDialogAddCarouselItem] = useState({
-        visible: false,
-    });
+  const [dialogAddCarouselItem, setDialogAddCarouselItem] = useState({
+    visible: false,
+  });
 
-    const closeDialogAddCarouselItem = useCallback(() => {
-        setDialogAddCarouselItem((prev) => ({ ...prev, visible: false }));
-    }, []);
+  const closeDialogAddCarouselItem = useCallback(() => {
+    setDialogAddCarouselItem((prev) => ({ ...prev, visible: false }));
+  }, []);
 
-    return (
-        <div className={`flex flex-column align-items-center  justify-content-center ${className}`}>
-            {!!images?.length && (
-                <Carousel
-                    pt={{ indicators: classNames("py-1") }}
-                    circular
-                    autoplayInterval={3000}
-                    showNavigators={false}
-                    value={images}
-                    numVisible={1}
-                    numScroll={1}
-                    itemTemplate={Image}
-                />
-            )}
-            <HasRequiredAuthority requiredAuthority={AUTHORITIES.MANAGE_FEATURE_CAROUSEL}>
-                <Button
-                    className="mt-1"
-                    icon="pi pi-plus"
-                    severity="warning"
-                    aria-label="Favorite"
-                    onClick={() => setDialogAddCarouselItem((prev) => ({ ...prev, visible: true, closeDialog: closeDialogAddCarouselItem }))}
-                    pt={{
-                        icon: { className: TEXT_SIZE_NORMAL },
-                    }}
-                />
-            </HasRequiredAuthority>
+  const responsiveOptions = [
+    {
+      breakpoint: "1400px",
+      numVisible: 2,
+      numScroll: 1,
+    },
+    {
+      breakpoint: "1199px",
+      numVisible: 2,
+      numScroll: 1,
+    },
+    {
+      breakpoint: "767px",
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
 
-            {dialogAddCarouselItem?.visible && (
-                <HasRequiredAuthority requiredAuthority={AUTHORITIES.MANAGE_FEATURE_CAROUSEL}>
-                    <DialogAddCarouselItem {...dialogAddCarouselItem} />
-                </HasRequiredAuthority>
-            )}
+  return (
+    <div
+      className={`flex flex-column align-items-center justify-content-center border-bottom-3 border-purple-500 shadow-2 bg-white border-round-bottom-xl overflow-hidden ${className}`}
+    >
+      {!!images?.length && (
+        <Carousel
+          value={images}
+          numVisible={2}
+          numScroll={1}
+          responsiveOptions={responsiveOptions}
+          className="w-full"
+          circular
+          autoplayInterval={3000}
+          showNavigators={true}
+          itemTemplate={Image}
+          pt={{
+            indicators: classNames("py-2"),
+            nextButton: classNames("bg-gray-800 text-white border-circle mx-2"),
+            previousButton: classNames(
+              "bg-gray-800 text-white border-circle mx-2"
+            ),
+          }}
+        />
+      )}
+
+      <HasRequiredAuthority
+        requiredAuthority={AUTHORITIES.MANAGE_FEATURE_CAROUSEL}
+      >
+        <div className="p-2">
+          <Button
+            className="p-button-sm border-round-lg shadow-2"
+            label="Add Slider Item"
+            icon="pi pi-plus"
+            severity="warning"
+            onClick={() =>
+              setDialogAddCarouselItem((prev) => ({
+                ...prev,
+                visible: true,
+                closeDialog: closeDialogAddCarouselItem,
+              }))
+            }
+            pt={{
+              icon: { className: TEXT_SIZE_NORMAL },
+            }}
+          />
         </div>
-    );
+      </HasRequiredAuthority>
+
+      {dialogAddCarouselItem?.visible && (
+        <HasRequiredAuthority
+          requiredAuthority={AUTHORITIES.MANAGE_FEATURE_CAROUSEL}
+        >
+          <DialogAddCarouselItem {...dialogAddCarouselItem} />
+        </HasRequiredAuthority>
+      )}
+    </div>
+  );
 }
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,32 +3,51 @@ import Operations from "../components/dashboard/Operations";
 import { useAppContext } from "../providers/ProviderAppContainer";
 import { Badge } from "primereact/badge";
 import CarouselImages from "../components/dashboard/CarouselImages";
-import { TEXT_SIZE_SMALL, TITLE_TEXT } from "../style";
+import { TEXT_SIZE_NORMAL } from "../style";
 
 export default function Dashboard() {
-    const { isDevelopmentBuild, deviceFingerPrint } = useAppContext();
+  const { isDevelopmentBuild, deviceFingerPrint } = useAppContext();
 
-    const pageConfig = useSelector((state) => state.stateTemplateConfig?.dash_board);
+  const pageConfig = useSelector(
+    (state) => state.stateTemplateConfig?.dash_board
+  );
 
-    return (
-        <div className="flex flex-column h-full overflow-hidden">
-            <div className="bg-blue-800 shadow-3 text-white flex align-items-center justify-content-between p-2">
-                <div className="w-8">
-                    <p className={`${TITLE_TEXT} m-0 font-semibold`}>Welcome To Sahas Smart Studies</p>
-                    {isDevelopmentBuild && (
-                        <div className={`${TEXT_SIZE_SMALL} mt-1 bg-blue-800 text-white white-space-nowrap text-overflow-ellipsis overflow-hidden`}>
-                            Device ID - {deviceFingerPrint}
-                        </div>
-                    )}
-                </div>
-
-                <i className="pi pi-bell p-overlay-badge mr-2" style={{ fontSize: "1.5rem" }}>
-                    <Badge value="2" severity="warning"></Badge>
-                </i>
+  return (
+    <div className="flex flex-column h-full overflow-hidden">
+      <div className="bg-blue-800 shadow-3 text-white flex align-items-center justify-content-between p-2 lg:px-8 ">
+        <div className="w-8">
+          <p
+            className={`text-base sm:text-lg md:text-lg lg:text-2xl m-0 font-semibold`}
+          >
+            Welcome To Sahas Smart Studies
+          </p>
+          {isDevelopmentBuild && (
+            <div
+              className={`text-xs sm:text-sm md:sm lg:text-lg mt-1 bg-blue-800 text-white white-space-nowrap text-overflow-ellipsis overflow-hidden`}
+            >
+              Device ID - {deviceFingerPrint}
             </div>
-
-            <CarouselImages className={"mb-2"} images={pageConfig?.carousel_images} />
-            <Operations className={"mx-2 mt-2 flex-1 min-h-0 overflow-scroll"} />
+          )}
         </div>
-    );
+        <div>
+          <i className={`pi pi-bell p-overlay-badge mr-2 ${TEXT_SIZE_NORMAL} `}>
+            <Badge value="2" severity="warning"></Badge>
+          </i>
+          <i
+            className={`pi pi-send p-overlay-badge mr-2 mx-5 ${TEXT_SIZE_NORMAL} `}
+          >
+            <Badge value="2" severity="warning"></Badge>
+          </i>
+        </div>
+      </div>
+
+      <CarouselImages
+        className={"mb-2 lg:px-8 "}
+        images={pageConfig?.carousel_images}
+      />
+      <Operations
+        className={"mx-2 mt-2 lg:px-8 flex-1 min-h-0 overflow-scroll"}
+      />
+    </div>
+  );
 }
