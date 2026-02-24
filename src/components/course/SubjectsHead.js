8f2bad51fa08712e181db862ad889d6fc7604b1c
nisarg <hammerbyte.nisarg@gmail.com>
2026-02-25 00:45:59 +0530

    whatsapp big icon

--- src/components/course/SubjectsHead.js
+++ src/components/course/SubjectsHead.js
@@ -130,35 +130,40 @@ export default function SubjectsHead({ course, subjects, setSubjects, updatingVi
 
             <div className="px-3 py-2 bg-blue-900 text-white flex gap-2 flex-column">
                 {!!(course?.enrollment?.on_site_access || course?.enrollment?.digital_access) && (
-                    <TabHeader
-                        title="Enrollment Details"
-                        highlights={[
-                            `Validity - ${getReadableDate({ date: course?.enrollment?.start_date, removeTime: true })} to ${getReadableDate({
-                                date: course?.enrollment?.end_date,
-                                removeTime: true,
-                            })}`,
-                            [
-                                !!course?.enrollment?.on_site_access ? "On-Site Access" : "No On-Site Access",
-                                !!course?.enrollment?.digital_access ? "Digital Access" : "No Digital Access",
-                            ].join(" & "),
-                        ]}
-                        actionItems={[
-                            <Button
-                                onClick={() => window.open(`https://chat.whatsapp.com/${course?.whatsapp_group}`)}
-                                icon="pi pi-whatsapp"
-                                rounded
-                                severity="success"
-                                aria-label="Join Whatsapp Group"
-                            />,
-                            <Button
-                                onClick={() => navigate(`/manage-users/${loggedInUser?.id}/enrollments`)}
-                                icon="pi pi-receipt"
-                                rounded
-                                severity="info"
-                                aria-label="Invoices"
-                            />,
-                        ]}
-                    />
+                    <div>
+                        <TabHeader
+                            title="Enrollment Details"
+                            highlights={[
+                                `Validity - ${getReadableDate({ date: course?.enrollment?.start_date, removeTime: true })} to ${getReadableDate({
+                                    date: course?.enrollment?.end_date,
+                                    removeTime: true,
+                                })}`,
+                                [
+                                    !!course?.enrollment?.on_site_access ? "On-Site Access" : "No On-Site Access",
+                                    !!course?.enrollment?.digital_access ? "Digital Access" : "No Digital Access",
+                                ].join(" & "),
+                            ]}
+                            actionItems={[
+                                <Button
+                                    onClick={() => navigate(`/manage-users/${loggedInUser?.id}/enrollments`)}
+                                    icon="pi pi-receipt"
+                                    rounded
+                                    severity="info"
+                                    aria-label="Invoices"
+                                />,
+                            ]}
+                        />
+                        <Button
+                            className="mt-2 w-full "
+                            onClick={() => window.open(`https://chat.whatsapp.com/${course?.whatsapp_group}`)}
+                            icon="pi pi-whatsapp"
+                            iconPos="right"
+                            severity="success"
+                            aria-label="Join Whatsapp Group"
+                            label="Join Whatsapp Group"
+                            size="small"
+                        />
+                    </div>
                 )}
 
                 {!course?.enrollment?.digital_access && (
